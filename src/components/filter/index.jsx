fd4ecf85fb54d42321eba5210d15b60dcf5a02e5
Dutra PC <joao.vitornl@gmail.com>
2024-01-06 23:16:29 -0300

    :construction: Integration dashbord and api

--- src/components/filter/index.jsx
+++ src/components/filter/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MultiSelectDropdown from '../Dropdown';
 import { Container } from './styles';
@@ -43,34 +43,31 @@ const data = [
 			"RVB-4I19"
 		],
 		selects:[]
-	},
-	{
-		name:"Conclusão",
-		options:[
-			"concluida",
-			"não Concluida",
-		],
-		selects:[]
 	}
 ]
-const Filter = () => {
+const Filter = ({data, onChange}) => {
 
+	const [selects, setSelects] = useState({local:[], plate:[], tool:[], users:[], allActivities:[], start:'', end:''});
 return (
 		<Container>
-			{
-				data.map((item, i) => {
-					return (
-						<div className="item" key={i}>
-							<MultiSelectDropdown { ...item}/>
-						</div>
-					)
-				})
-			}
 			<div className="item">
-				<DateInput/>
+				<MultiSelectDropdown name='Local' options={data.local ? data.local?.map(x => x?.name) : []} onChange={(x) => onChange({...selects, local: x}) & setSelects({...selects, local: x})}/>
 			</div>
 			<div className="item">
-				<DateInput/>
+				<MultiSelectDropdown name='Ferramenta'  options={data.tool ? data.tool?.map(x => x?.name) : []} onChange={(x) => onChange({...selects, tool: x}) & setSelects({...selects, tool: x})}/>
+			</div>
+			<div className="item">
+				<MultiSelectDropdown name='Responsavel'  options={data.users ? data.users?.map(x => x?.name) : []} onChange={(x) => onChange({...selects, users: x}) & setSelects({...selects, users: x})}/>
+			</div>
+			<div className="item">
+				<MultiSelectDropdown name='Atividade'  options={data.allActivities ? data.allActivities?.map(x => x) : []} onChange={(x) => onChange({...selects, allActivities: x}) & setSelects({...selects, allActivities: x})}/>
+			</div>
+
+			<div className="item">
+				<DateInput  autoDate onChange={(x) => onChange({...selects, start: x}) & setSelects({...selects, start: x})}/>
+			</div>
+			<div className="item">
+				<DateInput  autoDate onChange={(x) => onChange({...selects, end: x}) & setSelects({...selects, end: x})}/>
 			</div>
 		</Container>
 	)
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,54 +1,118 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
+import { toast } from 'react-toastify';
 
 import Layout from '../../components/layout';
 import Filter from '../../components/filter';
 import { Container, DataContainer, BoxDataContainer, Row} from './styles';
 
 import GraficoPizza from './charts/pizza';
+import api from '../../services/api';
 import Barra from './charts/barra';
 
 const Dashboard = () => {
-
-    const [data, setData] =  useState({
+    const [selects, setSelects] = useState({local:[], plate:[], tool:[], users:[], allActivities:[], start:'', end:''});
+    const [data, setData] = useState({local:[], plate:[], tool:[], users:[], statistics:{}, allActivities:[]});
+    const [titles, setTitles] =  useState({
         singleValues: [
             {
-                value: "190",
-                description: "Total de atividades prevista"
+                value: '0',
+                description: "Total de atividades"
             },
             {
-                value: "178",
+                value: "0",
                 description: "Atividades realizadas"
             },
             {
-                value: "12",
+                value: "0",
                 description: "Atividades não realizadas"
             },
             {
-                value: "10",
+                value: '0',
                 description: "Atividades não planejadas"
             },
             {
-                value: "6.000L",
+                value: "0.000L",
                 description: "Consumo médio de agua"
             },
             {
-                value: "78.000L",
+                value: "0.000L",
                 description: "Consumo total de agua"
             },
             {
-                value: "9.6/10",
+                value: "0/0",
                 description: "Aderencia"
             },
         ]
     })
 
+
+        const getData = async () => {
+            try {
+                const responses = await Promise.all([
+                    api.get('/local/'),
+                    api.get('/plate/'),
+                    api.get('/tool/'),
+                    api.get('/users/'),
+                    api.get('/statistics/')
+                ]);
+                
+                const [local, plate, tool, users, statistics] = responses.map(response => response.data);
+                var allActivities =  plate.reduce((acc, plate) => acc.concat(plate.timeline.map(activity => activity.name)), [])
+                setData({ local, plate, tool, users, statistics,  allActivities});
+            } catch (error) {
+                console.log(error);
+                toast.error("Erro interno ao puxar os dados do formulário")
+            }
+          };
+
+          const updateStatistics = async () => {
+            console.log('update')
+            var response = await api.get('/statistics/', { start: selects.start, end: selects.end});
+            response = response.data;
+            setData({...data, statistics: response});
+            setTitles({
+                singleValues: [
+                    {
+                        value: response.totalActivityCount || 0,
+                        description: "Total de atividades"
+                    },
+                    {
+                        value: response.scheduledActivityCount || 0,
+                        description: "Atividades dentro do cronograma"
+                    },
+                    {
+                        value: response.unscheduledActivityCount || 0,
+                        description: "Atividades fora do cronograma"
+                    },
+                    {
+                        value: response.averageWaterConsumption  + 'L'|| 0 ,
+                        description: "Consumo médio de agua"
+                    },
+                    {
+                        value: response.totalWaterConsumption  + 'L'|| 0 ,
+                        description: "Consumo total de agua"
+                    },
+                    {
+                        value: response.adherence || '10/10' ,
+                        description: "Aderencia"
+                    },
+                ]
+            })
+          };
+
+          
+          useEffect(() => { getData() }, [])
+          useEffect(() => updateStatistics, [selects])
+
+          
+
     return (
         <Layout initialSelect="Dashboard">
-             <Filter />
+             <Filter data={data} onChange={x => setSelects(x)}/>
              <Container>
                 <DataContainer>
                      {
-                        data.singleValues.map((item, i) => (
+                        titles.singleValues?.map((item, i) => (
                             <BoxDataContainer key={i}>
                                     <p>{item.value}</p>
                                     <p>{item.description}</p>
@@ -57,37 +121,41 @@ const Dashboard = () => {
                      }
                 </DataContainer>
                 <Row>
-                    <GraficoPizza name={'Uso nas atividades'} data={
-                        [
-                            ['tipo', 'quantidade'],
-                            ['bico de pato', 78],
-                            ['mangote', 28],
-                            ['mangote ab', 25]
-                        ]
-                    }/>
-                    <GraficoPizza name={'Conclusão das atividades'} data={
-                        [
-                            ['tipo', 'quantidade'],
-                            ['concluida', 178],
-                            ['não concluida', 12],
+                <GraficoPizza
+    name={'Uso de ferramentas'}
+    data={[
+        ['ferramenta', 'quantidade'],
+        ...(data.statistics.activityCountByTool
+            ? Object.entries(data.statistics.activityCountByTool).map(([key, value]) => [key, value])
+            : [])
+    ]}
+/>
+<GraficoPizza
+    name={'Uso de agua por atividade'}
+    data={[
+        ['ferramenta', 'quantidade'],
+        ...(data.statistics.waterConsumptionByActivity
+            ? Object.entries(data.statistics.waterConsumptionByActivity).map(([key, value]) => [key, value])
+            : [])
+    ]}
+/>
 
-                        ]
-                    }/>
+
                 </Row>
                 <Row>
-                    <GraficoPizza name={'Cronograma'} data={
+                    <GraficoPizza name={'Atividades do cronograma'} data={
                         [
                             ['tipo', 'quantidade'],
-                            ['atividades não planejadas', 10],
-                            ['atividades planejadas', 178],
+                            ['atividades dentro do cronograma', data?.statistics?.scheduledActivityCount],
+                            ['atividades fora do crnograma', data?.statistics?.unscheduledActivityCount],
 
                         ]
                     }/>
-                    <Barra name="Conclusão por local" data={[
-                        ['Categorias', 'Valores'],
-                        ['local 1', 30],
-                        ['local 2', 50],
-                        ['local 3', 20]
+                    <Barra name="Atividades por placa" data={[
+                        ['Quantidade', 'Placas'],
+                        ...(data.statistics.activityCountByPlate
+                            ? Object.entries(data.statistics.activityCountByPlate).map(([key, value]) => [key, value])
+                            : [])
                     ]}/>
                     
                 </Row>
