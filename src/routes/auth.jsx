32f5a40ae27d27c7da08b95edbc85ed6201295cd
dutra-pc <joao.vitornl@gmail.com>
2024-04-10 13:52:49 -0300

    :sparkles: feat: Create auth routes

---
+++ src/routes/auth.jsx
@@ -0,0 +1,14 @@
+import { Navigate } from 'react-router-dom';
+import React from 'react';
+
+const Auth = ({ element }) => {
+  const isAuthenticated = localStorage.getItem('token') !== null;
+
+  return isAuthenticated ? (
+    element
+  ) : (
+    <Navigate to="/signin" replace />
+  );
+};
+
+export default Auth;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -14,25 +14,59 @@ import Main from '../pages/main';
 import Book from "../pages/book";
 import SignIn from "../pages/signin";
 
+import Auth from "./auth";
+
   export const Router = () => {
     return(
       <BrowserRouter>
         <Routes>
           <Route path="*" element={<Navigate to="/404" replace/>}/>
-          <Route path="/dashboard" element={<Dashboard />}/>
-          <Route path="/link/dashboard" element={<Link />}/>
-          <Route path="/segurance/dashboard" element={<Segurance />}/>
-          <Route path="/formulario" element={<Formulario />}/>
-          <Route path="/equipamentos" element={<Equipamentos />}/>
-          <Route path="/veiculos" element={<Plate />}/>
-          <Route path="/equipe" element={<Equipe />}/>
-          <Route path="/relatorio" element={<Relatorio />}/>
-          <Route path="/local" element={<Local />}/>
-          <Route path="/404" element={<Error />}/>
-          <Route path="/signin" element={<SignIn />}/>
-          <Route path="/" element={<Main />}/>
           <Route path="/invite/:id" element={<Main />}/>
-          <Route path="/book" element={<Book />}/>
+          <Route path="/signin" element={<SignIn />}/>
+          <Route path="/404" element={<Error />}/>
+          <Route path="/" element={<Main />}/>
+
+          <Route
+  path="/dashboard"
+  element={<Auth element={<Dashboard />} />}
+/>
+<Route
+  path="/link/dashboard"
+  element={<Auth element={<Link />} />}
+/>
+<Route
+  path="/segurance/dashboard"
+  element={<Auth element={<Segurance />} />}
+/>
+<Route
+  path="/formulario"
+  element={<Auth element={<Formulario />} />}
+/>
+<Route
+  path="/equipamentos"
+  element={<Auth element={<Equipamentos />} />}
+/>
+<Route
+  path="/veiculos"
+  element={<Auth element={<Plate />} />}
+/>
+<Route
+  path="/equipe"
+  element={<Auth element={<Equipe />} />}
+/>
+<Route
+  path="/relatorio"
+  element={<Auth element={<Relatorio />} />}
+/>
+<Route
+  path="/local"
+  element={<Auth element={<Local />} />}
+/>
+<Route
+  path="/book"
+  element={<Auth element={<Book />} />}
+/>
+
         </Routes>
       </BrowserRouter>
     )
