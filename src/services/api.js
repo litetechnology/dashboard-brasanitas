27c7702735e6c846777c2bb5518ebcb7f073281d
dutra-pc <joao.vitornl@gmail.com>
2024-05-17 18:28:02 -0300

    :construction: Update baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,15 @@
 import axios from 'axios';
-import https from 'https'; 
 
 import config from '../assets/config';
 
 var token = localStorage.getItem('token');
-
-const httpsAgent = new https.Agent({  
-  rejectUnauthorized: false
-});
 
 const api = axios.create({
   baseURL: config.production ? config.baseURL : config.baseURL_test,
   headers: {
     'Content-Type': 'application/json',
     'authorization': token
-  },
-  httpsAgent: httpsAgent 
+  }
 });
 
 export default api;
